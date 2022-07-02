c64ae2c1350bc484eee104f24af2db101743ad32
Matt <ndovallesilva@gmail.com>
2022-07-02 15:14:13 -0300

    iniciando contatos

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Contatos } from './src/screens/Contatos/index'
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Contatos</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <Text>Constatos</Text>
+        <Text>Douglas Moreira</Text>
+        <Text>Marcelo Pessanha</Text>
+        <Text>Maria Eduarda Carvalho</Text>
+        <Text>Matheus Valle</Text>
+        <Text>Rodrigo Ramos</Text>
+        <Text>Rayssa Medeiros</Text>
+        <Text>Thatto Filgueiras</Text>
+        <StatusBar style="auto" />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+export default App
---
+++ src/screens/Contatos/index.js
@@ -0,0 +1,18 @@
+import react from "react"
+import { View, Text } from "react-native-web"
+
+const Contatos = () => {
+    return(
+        <View>
+            <Text>Constatos</Text>
+            <Text>Douglas Moreira</Text>
+            <Text>Marcelo Pessanha</Text>
+            <Text>Maria Eduarda Carvalho</Text>
+            <Text>Matheus Valle</Text>
+            <Text>Rodrigo Ramos</Text>
+            <Text>Rayssa Medeiros</Text>
+            <Text>Thatto Filgueiras</Text>
+        </View>
+    )
+}
+export default Contatos
