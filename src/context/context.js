4d25018185acdf9a7032daa0f84e20292d6cca21
Rayssa <rayssamt60@gmail.com>
2022-07-04 18:30:17 -0300

    rotas

--- src/context/context.js
+++ src/context/context.js
@@ -1,5 +1,5 @@
 import { createContext, useState } from "react";
-import { login } from "../services/auth";
+//import { login } from "../services/auth";
 
 export const AuthContext = createContext({});
 
