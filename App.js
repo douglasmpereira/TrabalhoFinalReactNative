8e7159ebe009a3d9994594d3c4f0257bd7cc09f0
Rayssa <rayssamt60@gmail.com>
2022-07-04 13:15:38 -0300

    navegação e rotas

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AuthProvider from './src/context/context';
 import Routes from './src/Routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Routes />
-      <StatusBar style="auto" />
-    </View>
+    <AuthProvider> {/*preciso ter um view? o provider substitui? */}
+      <View style={styles.container}>
+        <Routes />
+        <StatusBar style="auto" />
+      </View>
+    </AuthProvider>
   );
 }
 
