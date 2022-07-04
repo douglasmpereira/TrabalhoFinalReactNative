7e17cb783c7d3b1923e73a5268fd57177a2fcb8b
Matt <ndovallesilva@gmail.com>
2022-07-04 20:12:11 -0300

    acerto no contato

--- App.js
+++ App.js
@@ -1,130 +1,121 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StatusBar, Text, View, Image } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import React from 'react';
+import { ScrollView } from 'react-native';
+import { styles } from './styles'
 
 
 const App = () => {
   return (
+    <ScrollView contentContainerStyle={styles.container}>
 
-    <View style={styles.container}>
-      <>
-        <View style={styles.card}>
-          <>
-            <Text style={styles.baseText}>Douglas Moreira</Text>
-            <View style={styles.icon}>
-              <Icon name="facebook" size={30} color="white" />
-              <Icon name="instagram" size={30} color="white" />
-              <Icon name="linkedin" size={30} color="white" />
-              <Icon name="github" size={30} color="white" />
-            </View>
-          </>
+      <View>
+        <Text style={styles.titulo}>Contatos</Text>
+      </View>
+
+      <Image style={styles.img} source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }} />
+      <View style={styles.card}>
+        <Text style={styles.baseText}>Douglas Moreira</Text>
+        <View style={styles.icon}>
+          <Icon name="facebook" size={30} color="white" />
+          <Icon name="instagram" size={30} color="white" />
+          <Icon name="linkedin" size={30} color="white" />
+          <Icon name="github" size={30} color="white" />
         </View>
-        <View style={styles.card}>
-          <>
-            <Text style={styles.baseText}>Marcelo Pessanha</Text>
-            <View style={styles.icon}>
-              <Icon name="facebook" size={30} color="white" />
-              <Icon name="instagram" size={30} color="white" />
-              <Icon name="linkedin" size={30} color="white" />
-              <Icon name="github" size={30} color="white" />
-            </View>
-          </>
+      </View>
+
+
+      <Image style={styles.img} source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }} />
+
+      <View style={styles.card}>
+
+        <Text style={styles.baseText}>Marcelo Pessanha</Text>
+        <View style={styles.icon}>
+          <Icon name="facebook" size={30} color="white" />
+          <Icon name="instagram" size={30} color="white" />
+          <Icon name="linkedin" size={30} color="white" />
+          <Icon name="github" size={30} color="white" />
         </View>
-        <View style={styles.card}>
-          <>
-            <Text style={styles.baseText}>Maria Eduarda</Text>
-            <View style={styles.icon}>
-              <Icon name="facebook" size={30} color="white" />
-              <Icon name="instagram" size={30} color="white" />
-              <Icon name="linkedin" size={30} color="white" />
-              <Icon name="github" size={30} color="white" />
-            </View>
-          </>
+
+      </View>
+
+      <Image style={styles.img} source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }} />
+
+      <View style={styles.card}>
+
+        <Text style={styles.baseText}>Maria Eduarda</Text>
+        <View style={styles.icon}>
+          <Icon name="facebook" size={30} color="white" />
+          <Icon name="instagram" size={30} color="white" />
+          <Icon name="linkedin" size={30} color="white" />
+          <Icon name="github" size={30} color="white" />
         </View>
-        <View style={styles.card}>
-          <>
-            <Text style={styles.baseText}>Matheus Valle</Text>
-            <View style={styles.icon}>
-              <Icon name="facebook" size={30} color="white" />
-              <Icon name="instagram" size={30} color="white" />
-              <Icon name="linkedin" size={30} color="white" />
-              <Icon name="github" size={30} color="white" />
-            </View>
-          </>
+
+      </View>
+
+      <Image style={styles.img} source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }} />
+
+      <View style={styles.card}>
+
+        <Text style={styles.baseText}>Matheus Valle</Text>
+        <View style={styles.icon}>
+          <Icon name="facebook" size={30} color="white" />
+          <Icon name="instagram" size={30} color="white" />
+          <Icon name="linkedin" size={30} color="white" />
+          <Icon name="github" size={30} color="white" />
         </View>
-        <View style={styles.card}>
-          <>
-            <Text style={styles.baseText}>Rodrigo Ramos</Text>
-            <View style={styles.icon}>
-              <Icon name="facebook" size={30} color="white" />
-              <Icon name="instagram" size={30} color="white" />
-              <Icon name="linkedin" size={30} color="white" />
-              <Icon name="github" size={30} color="white" />
-            </View>
-          </>
+
+      </View>
+
+      <Image style={styles.img} source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }} />
+
+      <View style={styles.card}>
+
+        <Text style={styles.baseText}>Rodrigo Ramos</Text>
+        <View style={styles.icon}>
+          <Icon name="facebook" size={30} color="white" />
+          <Icon name="instagram" size={30} color="white" />
+          <Icon name="linkedin" size={30} color="white" />
+          <Icon name="github" size={30} color="white" />
         </View>
-        <View style={styles.card}>
-          <>
-            <Text style={styles.baseText}>Rayssa Medeiros</Text>
-            <View style={styles.icon}>
-              <Icon name="facebook" size={30} color="white" />
-              <Icon name="instagram" size={30} color="white" />
-              <Icon name="linkedin" size={30} color="white" />
-              <Icon name="github" size={30} color="white" />
-            </View>
-          </>
+
+      </View>
+
+      <Image style={styles.img} source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }} />
+
+      <View style={styles.card}>
+
+        <Text style={styles.baseText}>Rayssa Medeiros</Text>
+        <View style={styles.icon}>
+          <Icon name="facebook" size={30} color="white" />
+          <Icon name="instagram" size={30} color="white" />
+          <Icon name="linkedin" size={30} color="white" />
+          <Icon name="github" size={30} color="white" />
         </View>
-        <View style={styles.card}>
-          <>
-            <Text style={styles.baseText}>Thatto Filgueiras</Text>
-            <View style={styles.icon}>
-              <Icon name="facebook" size={30} color="white" />
-              <Icon name="instagram" size={30} color="white" />
-              <Icon name="linkedin" size={30} color="white" />
-              <Icon name="github" size={30} color="white" />
-            </View>
-          </>
+
+      </View>
+
+      <Image style={styles.img} source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }} />
+
+      <View style={styles.card}>
+
+        <Text style={styles.baseText}>Thatto Filgueiras</Text>
+        <View style={styles.icon}>
+          <Icon name="facebook" size={30} color="white" />
+          <Icon name="instagram" size={30} color="white" />
+          <Icon name="linkedin" size={30} color="white" />
+          <Icon name="github" size={30} color="white" />
         </View>
-      </>
+
+      </View>
+
       <StatusBar style="auto" />
-    </View>
 
+    </ScrollView>
 
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
 
-  },
-  card: {
-    backgroundColor: 'purple',
-    width: "50%",
-    height: "10%",
-    justifyContent: 'space-evenly',
-    display: "flex",
-    flexDirection: 'clolumn',
-    marginBottom: 10
 
-  },
-  baseText: {
-    color: 'white',
-    fontSize: "25%",
-    alignSelf: 'center'
-
-  },
-  icon: {
-    display: "flex",
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    marginBottom: '2%'
-
-  }
-});
-
-export default App
+export default App;
---
+++ styles.js
@@ -0,0 +1,45 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      padding:20  
+  
+    },
+    titulo:{
+        fontSize:50,
+        marginBottom: 10
+    },
+    card: {
+      backgroundColor: 'rgba(64, 0, 80, 0.7)',
+      width: "50%",
+      height: "10%",
+      justifyContent: 'space-evenly',     
+      marginBottom:20,
+      marginTop:5
+  
+    },
+    card1:{
+        
+    },
+    baseText: {
+      color: 'white',
+      fontSize: 25,
+      alignSelf: 'center'
+  
+    },
+    icon: {
+      display: "flex",
+      flexDirection: 'row',
+      justifyContent: 'space-evenly',
+      marginBottom: '2%'
+    },
+    img:{
+      width:100,
+      height:100,
+     
+    }
+    })  ;
+
