75d94c19d17c8eafd989f357aca02122f435ddd3
Matt <ndovallesilva@gmail.com>
2022-07-04 17:00:19 -0300

    cards com nomes

--- App.js
+++ App.js
@@ -1,26 +1,130 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import AuthProvider from './src/context/context';
-import Routes from './src/Routes';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import React from 'react';
+
 
 const App = () => {
   return (
-    <AuthProvider> {/*preciso ter um view? o provider substitui? */}
-      <View style={styles.container}>
-        <Routes />
-        <StatusBar style="auto" />
-      </View>
-    </AuthProvider>
+
+    <View style={styles.container}>
+      <>
+        <View style={styles.card}>
+          <>
+            <Text style={styles.baseText}>Douglas Moreira</Text>
+            <View style={styles.icon}>
+              <Icon name="facebook" size={30} color="white" />
+              <Icon name="instagram" size={30} color="white" />
+              <Icon name="linkedin" size={30} color="white" />
+              <Icon name="github" size={30} color="white" />
+            </View>
+          </>
+        </View>
+        <View style={styles.card}>
+          <>
+            <Text style={styles.baseText}>Marcelo Pessanha</Text>
+            <View style={styles.icon}>
+              <Icon name="facebook" size={30} color="white" />
+              <Icon name="instagram" size={30} color="white" />
+              <Icon name="linkedin" size={30} color="white" />
+              <Icon name="github" size={30} color="white" />
+            </View>
+          </>
+        </View>
+        <View style={styles.card}>
+          <>
+            <Text style={styles.baseText}>Maria Eduarda</Text>
+            <View style={styles.icon}>
+              <Icon name="facebook" size={30} color="white" />
+              <Icon name="instagram" size={30} color="white" />
+              <Icon name="linkedin" size={30} color="white" />
+              <Icon name="github" size={30} color="white" />
+            </View>
+          </>
+        </View>
+        <View style={styles.card}>
+          <>
+            <Text style={styles.baseText}>Matheus Valle</Text>
+            <View style={styles.icon}>
+              <Icon name="facebook" size={30} color="white" />
+              <Icon name="instagram" size={30} color="white" />
+              <Icon name="linkedin" size={30} color="white" />
+              <Icon name="github" size={30} color="white" />
+            </View>
+          </>
+        </View>
+        <View style={styles.card}>
+          <>
+            <Text style={styles.baseText}>Rodrigo Ramos</Text>
+            <View style={styles.icon}>
+              <Icon name="facebook" size={30} color="white" />
+              <Icon name="instagram" size={30} color="white" />
+              <Icon name="linkedin" size={30} color="white" />
+              <Icon name="github" size={30} color="white" />
+            </View>
+          </>
+        </View>
+        <View style={styles.card}>
+          <>
+            <Text style={styles.baseText}>Rayssa Medeiros</Text>
+            <View style={styles.icon}>
+              <Icon name="facebook" size={30} color="white" />
+              <Icon name="instagram" size={30} color="white" />
+              <Icon name="linkedin" size={30} color="white" />
+              <Icon name="github" size={30} color="white" />
+            </View>
+          </>
+        </View>
+        <View style={styles.card}>
+          <>
+            <Text style={styles.baseText}>Thatto Filgueiras</Text>
+            <View style={styles.icon}>
+              <Icon name="facebook" size={30} color="white" />
+              <Icon name="instagram" size={30} color="white" />
+              <Icon name="linkedin" size={30} color="white" />
+              <Icon name="github" size={30} color="white" />
+            </View>
+          </>
+        </View>
+      </>
+      <StatusBar style="auto" />
+    </View>
+
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'lightblue',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
+  card: {
+    backgroundColor: 'purple',
+    width: "50%",
+    height: "10%",
+    justifyContent: 'space-evenly',
+    display: "flex",
+    flexDirection: 'clolumn',
+    marginBottom: 10
+
+  },
+  baseText: {
+    color: 'white',
+    fontSize: "25%",
+    alignSelf: 'center'
+
+  },
+  icon: {
+    display: "flex",
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    marginBottom: '2%'
+
+  }
 });
 
 export default App
